feca8d7491d80bcc3949970623469360433bc165
tiffany <tiffany831101@gmail.com>
2018-12-01 10:34:25 +0800

    Updates

---
+++ precache-manifest.68e5ad55e88dec47250022b57df74b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77868f616b63da4b2fc7",
+    "url": "/first-react-app/static/js/runtime~main.77868f61.js"
+  },
+  {
+    "revision": "04c25781fed44eff99c0",
+    "url": "/first-react-app/static/js/main.04c25781.chunk.js"
+  },
+  {
+    "revision": "9895c1ec9258a0bb6164",
+    "url": "/first-react-app/static/js/1.9895c1ec.chunk.js"
+  },
+  {
+    "revision": "04c25781fed44eff99c0",
+    "url": "/first-react-app/static/css/main.bc23a10b.chunk.css"
+  },
+  {
+    "revision": "ba1a4713e48da21e61da0dfa4ed1332a",
+    "url": "/first-react-app/index.html"
+  }
+];
